402a34cca6cccb9afff4ba451f67d836b8db4623
John-Paul Drawneek <jpd@drawneek.co.uk>
2017-08-01 13:33:35 +0100

    Fixes and get the login user name

--- MAC/runme.sh
+++ MAC/runme.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters, you should enter INSTALL_TYPE [sre|php] then SSHKEY_PASSWORD"
+fi
+
+SSHKEY_PASSWORD=$2
+INSTALL_TYPE=$1
+
 SCRIPTPATH=`pwd -P`
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install ansible
 
-ansible-playbook $SCRIPTPATH/../ansible/playbook.yml
+ansible-playbook -e "sshkey_password=$SSHKEY_PASSWORD install_type=$INSTALL_TYPE" $SCRIPTPATH/../ansible/playbook.yml
 
